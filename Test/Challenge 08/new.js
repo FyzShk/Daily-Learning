576a43442988e5c4c17729117a0c18e65ef48567
FyzShk <fyzshk777@gmail.com>
2024-08-14 21:29:56 +0530

    Started Learning JS

---
+++ Test/Challenge 08/new.js
@@ -0,0 +1,46 @@
+
+
+//Quick Quiz solution
+// let o = {
+//     name : "Fayyaz",
+//     "phone number": "8767176815",
+//     marks : 97
+// }
+
+// console.log(o)
+
+// //Practice Set Question 1,2 solution
+// let a ="Fayyaz";
+// let b = 7;
+// // a ="Fayyaz2"
+
+// console.log(typeof a,typeof b)
+
+// //Practice Set Question 3,4 solution
+// const g = {
+//     name: "Fayyaz",
+//     "Pussy Smashed": 15,
+
+// }
+
+// console.log(g)
+
+// let g{
+//     "Girls Smashed":15,
+
+// }
+
+// console.log(g)
+
+////Practice Set Question 5 solution
+
+let dictionary = {
+    abductee : "a person who has been abducted",
+    abandonment : "an act or instance of leaving a person or thing permanently and completely",
+    abandonment: "an act or instance of leaving a person or thing permanently and completely",
+    abatement: "the act or state of abating or the state of being abated; reduction; decrease; alleviation; mitigation.",
+    abnormalize: "to make abnormal."
+
+}
+
+console.log(dictionary)
