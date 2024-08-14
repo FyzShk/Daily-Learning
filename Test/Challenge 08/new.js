8f9071063353263f30332be8d2a945e2dae04d80
FyzShk <fyzshk777@gmail.com>
2024-08-15 01:48:44 +0530

    Small changes

--- Test/Challenge 08/new.js
+++ Test/Challenge 08/new.js
@@ -3,7 +3,7 @@
 //Quick Quiz solution
 // let o = {
 //     name : "Fayyaz",
-//     "phone number": "8767176815",
+//     "phone number": "8518942645",
 //     marks : 97
 // }
 
@@ -19,14 +19,14 @@
 // //Practice Set Question 3,4 solution
 // const g = {
 //     name: "Fayyaz",
-//     "Pussy Smashed": 15,
+//     "Books read": 15,
 
 // }
 
 // console.log(g)
 
 // let g{
-//     "Girls Smashed":15,
+//     "Book":15,
 
 // }
 
