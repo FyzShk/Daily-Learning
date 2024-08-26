3412ec4bdc0c15f9fe44b087d93607d0983fd832
FyzShk <fyzshk777@gmail.com>
2024-08-26 16:49:33 +0530

    Faulty Calculator Attempt 1

--- Test/Challenge 09/index.js
+++ Test/Challenge 09/index.js
@@ -1,5 +1,38 @@
-console.log("Faulty Calculator")
+//Faulty Calculator
 
 
+let prompt = require('prompt-sync')();
+let random = Math.random();
+console.log(random)
+
+
+let a = prompt("Enter your first number: ")
+let b = prompt("Enter your second number: ")
+let c = prompt("Enter operation: ")
+
+let obj = {
+        "+": "-",
+        "-": "/",
+        "*": "+",
+        "/": "**"
+}
+
+// let result = `a`, `b`, `c``;
+
+
+if (random>0.1){
+        
+        console.log(a,c,b);
+        
+        
+
+}
+
+else {
+        c = obj[c],
+        console.log(a,c,b);        
+
+
+}
 
 
